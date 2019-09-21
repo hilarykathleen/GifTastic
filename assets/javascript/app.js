bb271a47066d797b6d5487eb400b37319aabf341
Hilary Lamb <hilaryklamb@gmail.com>
2019-09-21 19:54:34 -0400

    array buttons display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,30 @@
 //initial values
 
-// var apiKey: "hcQ4oS9ljqo3Thqq1MdugzfJYIYMQ2QJ";
-
 //Create an array of strings saved in variable topics
 
-var buttons = ["cat", "dog", "armadillo", "narwhal"];
+let buttons = ['cat', 'dog', 'armadillo', 'narwhal'];
 
 //function>for loop through array to create buttons for each element
 
 
-
-    function cloneButtons() {
+function displayButtons() {
         $('.recent-search').empty();
     
         for (let i = 0; i < buttons.length; i++) {
-            const buttonName = buttons[i];
-    
-            const button = `<div class="buttons">
-                    <button
-                        data-name="${buttonName}"
-                        class="btn btn-search"
-                    >
-                    ${buttonName}
-                    </button>
-                    <button
-                        data-name="${buttonName}"
-                        class="btn btn-close fas fa-times">
-                    </button>
-            </div>`;
-    
+            let buttonName = buttons[i];
+            console.log(buttonName);
+            
+            let button = `<div class="wrap-buttons">
+                    <button class="btn btn-search"
+                    data-name="${buttonName}">
+                    ${buttonName}</button>
+                    <button data-name="${buttonName}"
+                    class="btn btn-delete"></button>
+            `;
+
             $('.recent-search').append(button);
         }
-    
-}
+    }
+
+    displayButtons();
+                        
