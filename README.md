0c5615d8e56b3e45d2497192bf76421ed0dc9436
Hilary Lamb <hilaryklamb@gmail.com>
2019-09-21 16:47:42 -0400

    initial pseudo code, working on first function to display buttons in array-currently not working

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # GifTastic
+
+initial pseudocode
+
+Create an array of strings saved in variable topics
+Get api
+Choose a theme
+Loop appends a button for each string in an array
+On click function 
+Ajax grab giphy when clicked
+Static
+Dynamic function puts button on page static giphy
+Once button it pushed giphy will animate
+Form takes value from user input adds to topics array
+Function call topic in array remakes buttons on page
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,35 @@
-console.log($);
+//initial values
+
+// var apiKey: "hcQ4oS9ljqo3Thqq1MdugzfJYIYMQ2QJ";
+
+//Create an array of strings saved in variable topics
+
+var buttons = ["cat", "dog", "armadillo", "narwhal"];
+
+//function>for loop through array to create buttons for each element
+
+
+
+    function cloneButtons() {
+        $('.recent-search').empty();
+    
+        for (let i = 0; i < buttons.length; i++) {
+            const buttonName = buttons[i];
+    
+            const button = `<div class="buttons">
+                    <button
+                        data-name="${buttonName}"
+                        class="btn btn-search"
+                    >
+                    ${buttonName}
+                    </button>
+                    <button
+                        data-name="${buttonName}"
+                        class="btn btn-close fas fa-times">
+                    </button>
+            </div>`;
+    
+            $('.recent-search').append(button);
+        }
+    
+}
